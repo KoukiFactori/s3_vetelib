28da26af859fcb9d3bd45a4cee30746b9828ebe0
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-12-05 09:52:43 +0100

    Adaptation du formulaire en formulaire Symfony

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('contact/index.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());
+        }
+
+        return $this->renderForm('contact/index.html.twig', compact('form'));
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, options: [
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('firstname', TextType::class, options: [
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('email', EmailType::class, options: [
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
+            ->add('message', TextareaType::class, options: [
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 10]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
