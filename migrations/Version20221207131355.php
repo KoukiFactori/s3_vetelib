9360b2e971ec450ec3cf1a7ac9ae571c8c5779e1
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2022-12-07 14:13:55 +0100

    Create  migration

--- migrations/Version20221207131355.php
+++ migrations/Version20221207131355.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221207130603 extends AbstractMigration
+final class Version20221207131355 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,10 @@ final class Version20221207130603 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE animal (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, espece_id INTEGER NOT NULL, name VARCHAR(40) NOT NULL, birthdate DATE NOT NULL, CONSTRAINT FK_6AAB231F2D191E7A FOREIGN KEY (espece_id) REFERENCES espece (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE animal (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, espece_id INTEGER NOT NULL, client_id INTEGER NOT NULL, name VARCHAR(40) NOT NULL, birthdate DATE NOT NULL, CONSTRAINT FK_6AAB231F2D191E7A FOREIGN KEY (espece_id) REFERENCES espece (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_6AAB231F19EB6921 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_6AAB231F2D191E7A ON animal (espece_id)');
+        $this->addSql('CREATE INDEX IDX_6AAB231F19EB6921 ON animal (client_id)');
+        $this->addSql('CREATE TABLE client (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, lastname VARCHAR(50) NOT NULL, firstname VARCHAR(50) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) DEFAULT NULL, birthdate DATE NOT NULL, city VARCHAR(60) NOT NULL, zipcode VARCHAR(20) NOT NULL, address VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE TABLE espece (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(50) NOT NULL)');
         $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL)');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
@@ -33,6 +35,7 @@ final class Version20221207130603 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE animal');
+        $this->addSql('DROP TABLE client');
         $this->addSql('DROP TABLE espece');
         $this->addSql('DROP TABLE messenger_messages');
     }
