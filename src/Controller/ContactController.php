a98b269632acccbe35c25104e56fc7c387eb48aa
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-12-09 13:41:22 +0100

    ajout de l'envoie de mail lors d'un contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,18 +6,36 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($form->getData());
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from('contact@vetelib.fr')
+                ->to('simon@simon511000.fr')
+                ->subject('Vous avez reçu un message de la page contact')
+                ->text(<<<TXT
+                    Nom : {$contactFormData['lastname']}
+                    Prénom : {$contactFormData['firstname']}
+                    Adresse Mail : {$contactFormData['email']}
+                    Message : {$contactFormData['message']}
+                TXT)
+            ;
+
+            $mailer->send($message);
+
+            return $this->redirectToRoute('app_contact');
         }
 
         return $this->renderForm('contact/index.html.twig', compact('form'));
