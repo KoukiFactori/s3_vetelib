e86182b4d0f4d5c60021915f168b81257d87fd6b
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2022-12-19 21:25:42 +0100

    Modification du carrousel avec l'utilisation d'une librairie JS slick

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,11 @@
-// Récupère la balise <div> qui contient les images
-var carousel = document.getElementById("carousel");
-
-// Récupère les images contenues dans la balise <div>
-var images = carousel.getElementsByTagName("img");
-
-// Index de l'image à afficher
-var index = 0;
-
-// Fonction pour passer à l'image précédente
-function previous() {
-  // Masque l'image actuelle
-  images[index].style.display = "none";
-
-  // Décrémente l'index
-  index = (index - 1 + images.length) % images.length;
-
-  // Affiche l'image précédente
-  images[index].style.display = "block";
-}
-
-// Fonction pour passer à l'image suivante
-function next() {
-  // Masque l'image actuelle
-  images[index].style.display = "none";
-
-  // Incrémente l'index
-  index = (index + 1) % images.length;
-
-  // Affiche l'image suivante
-  images[index].style.display = "block";
-}
-
-// Fonction pour défiler image
-setInterval(next, 5000)
+$(document).ready(function () {
+  $('.carrousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
