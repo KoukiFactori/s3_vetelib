9a939af3d80b7037eca6ad145ca6768b8b4d1dd0
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-01-02 00:31:52 +0100

    ajout controller et template pour la page client animaux

---
+++ src/Controller/ClientAnimalsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AnimalRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientAnimalsController extends AbstractController
+{
+    #[Route('/client/animals', name: 'app_client_animals')]
+    public function index(AnimalRepository $ar): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_CLIENT');
+
+        $clientId=$this->getUser()->Id;
+        $animals=$ar->getAllAnimalsByClient($clientId);
+
+        return $this->render('client_animals/index.html.twig', ['animals' => $animals] );
+    
+    }
+}
