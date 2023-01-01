40a7c49f713c91515c64364127e36f4eeb0454ec
Teia <nicolas.mossmann@etudiant.univ-reims.fr>
2023-01-01 22:50:02 +0100

    Create app file

---
+++ assets/slider.js
@@ -0,0 +1,11 @@
+import slick from 'slick-carousel';
+
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
