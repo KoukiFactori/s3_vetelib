23a43a8a8b07cf91d1d47bf6aae91ef943e63736
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-02 19:24:46 +0100

    Création du controller

---
+++ src/Controller/ClientInformationsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientInformationsController extends AbstractController
+{
+    #[Route('/client/informations', name: 'app_client_informations')]
+    public function index(): Response
+    {
+        return $this->render('client_informations/index.html.twig', [
+            'controller_name' => 'ClientInformationsController',
+        ]);
+    }
+}
