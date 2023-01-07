b4468eff1b203e01b2a642c5c065ff382c7f22ea
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-07 17:31:44 +0100

    Persistance des données

--- src/Controller/ClientInformationsController.php
+++ src/Controller/ClientInformationsController.php
@@ -4,19 +4,28 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ClientInformationsController extends AbstractController
 {
     #[Route('/client/informations/{user}', name: 'app_client_informations', requirements: ['user' => "\d+"])]
-    public function index(User $user): Response
+    public function index(User $user, Request $request, UserRepository $userRepository): Response
     {
         $form = $this->createForm(UserType::class, $user);
-        //$this->denyAccessUnlessGranted('ROLE_CLIENT');
-        //$clientId=$this->getUser()->Id;
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
 
+            $userRepository->save($user, true);
+            return $this->redirectToRoute('/client/informations/2');
+        }
+        // $this->denyAccessUnlessGranted('ROLE_CLIENT');
+        // $clientId=$this->getUser()->Id;
 
         return $this->render('client/client_informations/index.html.twig', [
             'form' => $form->createView(), 'controller_name' => 'ClientInformationsController',
