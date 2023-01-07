bd14b996d0878a409b0f19025ca710d0ff40770e
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-07 23:26:19 +0100

    Ajout des events dans le calendrier

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -2,15 +2,44 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\EventRepository;
 use CalendarBundle\CalendarEvents;
+use CalendarBundle\Event\CalendarEvent;
+use CalendarBundle\Entity\Event;
+use DateTime;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
+    private EventRepository $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+    
     public static function getSubscribedEvents(): array
     {
         return [
             CalendarEvents::SET_DATA => 'onCalendarSetData',
         ];
+    }
+
+    public function onCalendarSetData(CalendarEvent $calendar)
+    {
+        $events = $this->eventRepository->getEventsBetween(
+            $calendar->getStart(),
+            $calendar->getEnd()
+        );
+
+        foreach($events as $event) {
+            $calendarEvent = new Event(
+                $event->getDescription(),
+                $event->getDate(),
+                DateTime::createFromInterface($event->getDate())->modify('+30 minutes')
+            );
+
+            $calendar->addEvent($calendarEvent);
+        }
     }
 }
