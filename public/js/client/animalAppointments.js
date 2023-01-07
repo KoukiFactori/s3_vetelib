d32e30570ed5ee1193fd26afa72203d3a2424af8
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-01-07 16:05:53 +0100

    ajout fetch pour tout les lien dans le js + test d'ajout des donnée fetch | ajout d'une classe en html pour que le js fonctionne | modification erreur controlleur

--- public/js/client/animalAppointments.js
+++ public/js/client/animalAppointments.js
@@ -1,2 +1,17 @@
 const animalLinks=document.querySelectorAll('.animal-link')
 
+animalLinks.forEach(link=>{
+    link.addEventListener('click',event =>{
+        event.preventDefault();
+
+        
+        
+        fetch("/client/animals/"+link.dataset.id,['credentials:include'])
+        .then(response=>response.json())
+        .then(data=>{
+            document.querySelector(".name").innerText = data.name
+        })
+        console.log("fdp")
+    })
+
+})
