f40af0822b7e7064d8a130fcfc14dcf93b0882fe
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-08 14:59:25 +0100

    Modification afin d'enregistrer les données de l'utilisateur connecté

--- src/Controller/ClientInformationsController.php
+++ src/Controller/ClientInformationsController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ClientInformationsController extends AbstractController
 {
-    #[Route('/client/informations/{user}', name: 'app_client_informations', requirements: ['user' => "\d+"])]
-    public function index(User $user, Request $request, UserRepository $userRepository): Response
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
     {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    #[Route('/client/informations', name: 'app_client_informations')]
+    public function index(UserRepository $userRepository, Request $request): Response
+    {
+        $token = $this->tokenStorage->getToken();
+        $user = $token->getUser();
+        $userId = $user->getId();
+        $user = $userRepository->find($userId);
+
         $form = $this->createForm(UserType::class, $user);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
@@ -23,7 +36,6 @@ class ClientInformationsController extends AbstractController
             $userRepository->save($user, true);
         }
         // $this->denyAccessUnlessGranted('ROLE_CLIENT');
-        //$clientId = $this->getUser()->id;
 
         return $this->render('client/client_informations/index.html.twig', [
             'form' => $form->createView(), 'controller_name' => 'ClientInformationsController',
