9be5ba122f0e1b364278cea31a1170c3144768f1
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-01-08 02:17:46 +0100

    ajout systeme d'annulation requetes js et asynchrone

--- src/Controller/ClientAnimalsController.php
+++ src/Controller/ClientAnimalsController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use App\Repository\AnimalRepository;
 use App\Repository\EventRepository;
-use phpDocumentor\Reflection\Types\AbstractList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class ClientAnimalsController extends AbstractController
