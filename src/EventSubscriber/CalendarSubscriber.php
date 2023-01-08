a2b1be9b0b57a88aaa77a7e6dd80749feb3aee99
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-08 17:25:44 +0100

    Ajout du lien de modification des Events dans le calendrier

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -4,23 +4,25 @@ namespace App\EventSubscriber;
 
 use App\Repository\EventRepository;
 use CalendarBundle\CalendarEvents;
-use CalendarBundle\Event\CalendarEvent;
 use CalendarBundle\Entity\Event;
-use DateTime;
+use CalendarBundle\Event\CalendarEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
     private EventRepository $eventRepository;
     private Security $security;
+    private UrlGeneratorInterface $router;
 
-    public function __construct(EventRepository $eventRepository, Security $security)
+    public function __construct(EventRepository $eventRepository, Security $security, UrlGeneratorInterface $router)
     {
         $this->eventRepository = $eventRepository;
         $this->security = $security;
+        $this->router = $router;
     }
-    
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -36,11 +38,18 @@ class CalendarSubscriber implements EventSubscriberInterface
             $calendar->getEnd()
         );
 
-        foreach($events as $event) {
+        foreach ($events as $event) {
             $calendarEvent = new Event(
                 $event->getDescription(),
                 $event->getDate(),
-                DateTime::createFromInterface( $event->getDate())->modify('+30 minutes')
+                \DateTime::createFromInterface($event->getDate())->modify('+30 minutes')
+            );
+
+            $calendarEvent->addOption(
+                'url',
+                $this->router->generate('app_panel_event_show', [
+                    'id' => $event->getId(),
+                ])
             );
 
             $calendar->addEvent($calendarEvent);
