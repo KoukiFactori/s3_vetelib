2f52b74eaa93fc26158299afc0d84f7cd269b9f4
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-08 13:29:28 +0100

    Restriction des évents aux vétérinaires

--- src/Controller/PanelEventController.php
+++ src/Controller/PanelEventController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_VETERINAIRE')]
 class PanelEventController extends AbstractController
 {
     #[Route('/panel/event', name: 'app_panel_event')]
