aa4ff0694de860d796a689338ef292be7bfda995
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-01-08 02:17:46 +0100

    ajout systeme d'annulation requetes js et asynchrone

--- public/js/client/animalAppointments.js
+++ public/js/client/animalAppointments.js
@@ -1,17 +1,29 @@
-const animalLinks=document.querySelectorAll('.animal-link')
+const animalLinks = document.querySelectorAll('.animal-link')
 
-animalLinks.forEach(link=>{
-    link.addEventListener('click',event =>{
+let abortController;
+
+animalLinks.forEach(link => {
+    link.addEventListener('click', async (event) => {
         event.preventDefault();
 
-        
-        
-        fetch("/client/animals/"+link.dataset.id,['credentials:include'])
-        .then(response=>response.json())
-        .then(data=>{
-            document.querySelector(".name").innerText = data.name
-        })
-        console.log("fdp")
-    })
+        if (abortController) {
+            abortController.abort();
+        }
 
-})
+        abortController = new AbortController();
+
+        const data = await fetch("/client/animals/" + link.dataset.id, {
+            signal: abortController.signal
+        })
+            .then(response => response.json());
+
+        const date = new Date(data.birthdate);
+        document.querySelector(".name").innerText = data.name
+        document.querySelector(".species").innerText = data.espece.name
+        document.querySelector(".birthdate").innerText = date.toLocaleDateString('fr-FR', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        });
+    })
+});
