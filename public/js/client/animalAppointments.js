d084ace693264d7606c0130f988870aba990fa45
Tom Raineri <tom.raineri@etudiant.univ-reims.fr>
2023-01-08 06:54:31 +0100

    ajout html pour les rendez vous | ajout css pour rendre le site coherent avec le figme | ajout javascript pour le html

--- public/js/client/animalAppointments.js
+++ public/js/client/animalAppointments.js
@@ -1,5 +1,6 @@
-const animalLinks = document.querySelectorAll('.animal-link')
-
+const animalLinks = document.querySelectorAll('.animal-link');
+const firstAnimal = document.querySelector('.animal-link');
+fetch("/client/animals/" + firstAnimal.dataset.id);
 let abortController;
 
 animalLinks.forEach(link => {
@@ -18,7 +19,7 @@ animalLinks.forEach(link => {
             .then(response => response.json());
 
         const birthday = new Date(data.birthdate);
-        const age =(new Date().getFullYear() - birthday.getFullYear()) ;
+        const age = (new Date().getFullYear() - birthday.getFullYear());
         document.querySelector(".name").innerText = data.name
         document.querySelector(".species").innerText = data.espece.name
         document.querySelector(".birthdate").innerText = birthday.toLocaleDateString('fr-FR', {
@@ -26,9 +27,23 @@ animalLinks.forEach(link => {
             month: 'long',
             year: 'numeric'
         });
-        document.querySelector(".age").innerText = age.toLocaleString('fr-FR', { 
-        year: 'numeric'
+        document.querySelector(".age").innerText = age.toLocaleString('fr-FR', {
+            year: 'numeric'
         });
-        
+        const appointments = document.querySelector(".appointments");
+        while (appointments.firstChild) appointments.removeChild(appointments.firstChild)
+        for (const event of data.events) {
+            const dateEvent = new Date(event.date).toLocaleDateString('fr-FR', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            });;
+            const element = document.createElement("li");
+            element.innerHTML = `
+            <p>${event.description}</p>
+            <p>${dateEvent}
+            `;
+            appointments.appendChild(element);
+        }
     })
 });
