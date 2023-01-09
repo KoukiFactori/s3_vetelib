0a6e889b255042c65eac6cb5253f220f418cddc3
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-09 16:11:36 +0100

    Ajout de l'animal en fonction de l'utilisateur connecté

--- src/Controller/ClientAnimalsAjoutController.php
+++ src/Controller/ClientAnimalsAjoutController.php
@@ -2,28 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Entity\Espece;
 use App\Form\AddAnimalType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ClientAnimalsAjoutController extends AbstractController
 {
+    
+    private $em;
     private $tokenStorage;
 
-    public function __construct(TokenStorageInterface $tokenStorage)
+    public function __construct(EntityManagerInterface $em, TokenStorageInterface $tokenStorage)
     {
+        $this->em = $em;
         $this->tokenStorage = $tokenStorage;
     }
 
-    
     #[Route('/mon_profil/animal/add', name: 'app_client_animals_ajout')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createForm(AddAnimalType::class);
-
+        $user = $this->tokenStorage->getToken()->getUser();
         
+        $form = $this->createForm(AddAnimalType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Création d'un animal à partir des informations données dans le formulaire
+            $animal = new Animal();
+            $animal->setName($form['name']->getData());
+            $animal->setBirthdate($form['birthdate']->getData());
+
+            $especeId = $form['espece']->getData();
+            $espece = $this->em->getRepository(Espece::class)->find($especeId);
+            $animal->setEspece($espece);
+
+            $animal->setClient($user);
+
+            
+            $this->em->persist($animal);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_client_animals');
+        }
+
         return $this->render('client/client_animals/client_animals_ajout/index.html.twig', [
             'form' => $form->createView(), 'controller_name' => 'ClientAnimalsAjoutController',
         ]);
--- src/Form/AddAnimalType.php
+++ src/Form/AddAnimalType.php
@@ -4,39 +4,35 @@ namespace App\Form;
 
 use App\Entity\Animal;
 use App\Entity\Espece;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class AddAnimalType extends AbstractType
 {
-    private $em;
+    private $tokenStorage;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(TokenStorageInterface $tokenStorage)
     {
-        $this->em = $em;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $especes = $this->em->getRepository(Espece::class)->findAll();
-        $choices = [];
-
-        foreach ($especes as $espece) {
-            $choices[$espece->getName()] = $espece->getId();
-        }
+        $user = $this->tokenStorage->getToken()->getUser();
 
         $builder
             ->add('name')
             ->add('birthdate')
-            ->add('espece', ChoiceType::class, [
-                'choices' => $choices,
+            ->add('espece', EntityType::class, [
+                'class' => Espece::class,
+                'choice_label' => 'name',
             ])
-            ->add('save', SubmitType::class)
-        ;
+            ->add('save', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
