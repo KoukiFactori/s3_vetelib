2b04ba1e8fbed9f2233a9496ed9cdad592fbd42c
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-09 12:35:15 +0100

    Création du controlleur du nouveau formulaire d'ajout

---
+++ src/Controller/ClientAnimalsAjoutController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientAnimalsAjoutController extends AbstractController
+{
+    #[Route('/mon_profil/animal/add', name: 'app_client_animals_ajout')]
+    public function index(): Response
+    {
+        return $this->render('client_animals_ajout/index.html.twig', [
+            'controller_name' => 'ClientAnimalsAjoutController',
+        ]);
+    }
+}
