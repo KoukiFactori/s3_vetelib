2c795a9dfc28a54c4eb3d56daf88963dc363a799
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-09 19:56:18 +0100

    Restriction de la page aux connectés

--- src/Controller/PrendreRDVController.php
+++ src/Controller/PrendreRDVController.php
@@ -5,24 +5,28 @@ namespace App\Controller;
 use App\Repository\EspeceRepository;
 use App\Repository\TypeEventRepository;
 use App\Repository\VeterinaireRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_CLIENT')]
 class PrendreRDVController extends AbstractController
 {
-    #[Route('/prendre/rdv', name: 'app_prendre_rdv')]
+    #[Route('/prendre-rdv', name: 'app_prendre_rdv')]
     public function index(EspeceRepository $especeRepo, TypeEventRepository $typeEventRepo, VeterinaireRepository $vetoRepo, Request $request): Response
     {
+        $selectedDate = $request->get('date', (new \DateTime())->format('d/m/Y'));
+        
         $especes = $especeRepo->getAll();
         $typeEvents = $typeEventRepo->getAll();
-        $vetoAndEvents = $vetoRepo->getEventsOn(new \DateTime());
+        $vetoAndEvents = $vetoRepo->getEventsOn(date_create_from_format('d/m/Y', $selectedDate));
         $slots = $vetoRepo->getAvailableSlots($vetoAndEvents);
 
         return $this->render('prendre_rdv/index.html.twig', [
             'selectedEspece' => $request->get('espece'),
-            'selectedDate' => (new \DateTime())->format('d/m/Y'),
+            'selectedDate' => $selectedDate,
             'selectedTypeEvent' => $request->get('typeEvent'),
             'especes' => $especes,
             'typeEvents' => $typeEvents,
