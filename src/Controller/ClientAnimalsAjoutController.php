c33c67b9b5eac7203af6757717d345e2648e0408
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-09 12:43:21 +0100

    Déplacement de la template

--- src/Controller/ClientAnimalsAjoutController.php
+++ src/Controller/ClientAnimalsAjoutController.php
@@ -11,7 +11,7 @@ class ClientAnimalsAjoutController extends AbstractController
     #[Route('/mon_profil/animal/add', name: 'app_client_animals_ajout')]
     public function index(): Response
     {
-        return $this->render('client_animals_ajout/index.html.twig', [
+        return $this->render('client/client_animals/client_animals_ajout/index.html.twig', [
             'controller_name' => 'ClientAnimalsAjoutController',
         ]);
     }
