034c7c6f487c84316b4f6a4581bd4393bf70226a
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-09 15:40:50 +0100

    Envoie de toutes les données dans la vue

--- src/Controller/PrendreRDVController.php
+++ src/Controller/PrendreRDVController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\EspeceRepository;
+use App\Repository\EventRepository;
 use App\Repository\TypeEventRepository;
+use App\Repository\VeterinaireRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PrendreRDVController extends AbstractController
 {
     #[Route('/prendre/rdv', name: 'app_prendre_rdv')]
-    public function index(EspeceRepository $especeRepo, TypeEventRepository $typeEventRepo): Response
+    public function index(EspeceRepository $especeRepo, TypeEventRepository $typeEventRepo, VeterinaireRepository $vetoRepo): Response
     {
         $especes = $especeRepo->getAll();
         $typeEvents = $typeEventRepo->getAll();
+        $vetoAndEvents = $vetoRepo->getEventsOn(new DateTime());
+        $slots = $vetoRepo->getAvailableSlots($vetoAndEvents);
 
-        return $this->render('prendre_rdv/index.html.twig', compact("especes", "typeEvents"));
+        return $this->render('prendre_rdv/index.html.twig',[
+            'especes' => $especes,
+            'typeEvents' => $typeEvents,
+            'vetos' => $vetoAndEvents,
+            'slots' => $slots
+        ]);
     }
 }
