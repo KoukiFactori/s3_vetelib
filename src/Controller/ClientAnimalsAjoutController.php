c24de4c0990ec8a723921952c9ed070ae75b821d
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-09 14:58:37 +0100

    Affichage du formulaire dans la vue + création de celui-ci

--- src/Controller/ClientAnimalsAjoutController.php
+++ src/Controller/ClientAnimalsAjoutController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\AddAnimalType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ClientAnimalsAjoutController extends AbstractController
 {
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    
     #[Route('/mon_profil/animal/add', name: 'app_client_animals_ajout')]
     public function index(): Response
     {
+        $form = $this->createForm(AddAnimalType::class);
+
+        
         return $this->render('client/client_animals/client_animals_ajout/index.html.twig', [
-            'controller_name' => 'ClientAnimalsAjoutController',
+            'form' => $form->createView(), 'controller_name' => 'ClientAnimalsAjoutController',
         ]);
     }
 }
