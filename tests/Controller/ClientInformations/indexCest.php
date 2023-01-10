834411cc1f2c5e86a1c9e29e4d9445580795c784
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-10 16:22:07 +0100

    Création du premier test Vérification que la page charge correctement une fois connecté

--- tests/Controller/ClientInformations/indexCest.php
+++ tests/Controller/ClientInformations/indexCest.php
@@ -2,21 +2,23 @@
 
 namespace App\Tests\Controller\ClientInformations;
 
-use App\Factory\UserFactory;
+use App\Entity\Client;
+use App\Factory\ClientFactory;
 use App\Tests\Support\ControllerTester;
 
-class indexCest
+class IndexCest
 {
     // Vérifier le bon chargement de la page
     public function chargementPage(ControllerTester $I)
     {
         // Création d'un utilisateur Client
-        $user = UserFactory::createOne(['lastname' => 'Ledoux', 'firstname' => 'Simon', 'email' => 'simon@simon511000.fr', 'roles' => ['ROLE_CLIENT'], 'password' => 'password','phone' => '0654685216', 'birthdate' => new \cr, 'city' => 'Paris', 'zipcode' => '75000', 'address' => '1 rue de la paix']);
+        $user = ClientFactory::createOne(['lastname' => 'Ledoux', 'firstname' => 'Simon', 'email' => 'simon@simon511000.fr', 'roles' => ['ROLE_CLIENT'], 'password' => 'password', 'phone' => '0654685216', 'birthdate' => new \DateTime('1970-01-01'), 'city' => 'Paris', 'zipcode' => '75000', 'address' => '1 rue de la paix']);
 
+        /** @var(Client) */
         $user = $user->object();
         $I->amLoggedInAs($user);
 
-        $I->amOnPage('/client-informations');
+        $I->amOnRoute('app_client_informations');
         $I->seeResponseCodeIs(200);
     }
 }
