1e7290340610371a36068185486decb47b042118
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-10 17:18:59 +0100

    Création test vérifier le remplissage des champs du formulaire

--- tests/Controller/ClientInformations/indexCest.php
+++ tests/Controller/ClientInformations/indexCest.php
@@ -21,4 +21,25 @@ class IndexCest
         $I->amOnRoute('app_client_informations');
         $I->seeResponseCodeIs(200);
     }
+
+    // Vérifier que le formulaire est bien rempli
+    public function remplissageFormulaire(ControllerTester $I)
+    {
+        // Création d'un utilisateur Client
+        $user = ClientFactory::createOne(['lastname' => 'Ledoux', 'firstname' => 'Simon', 'email' => 'simon2@simon511000.fr', 'roles' => ['ROLE_CLIENT'], 'password' => 'password', 'phone' => '0654685216', 'birthdate' => new \DateTime('1970-01-01'), 'city' => 'Paris', 'zipcode' => '75000', 'address' => '1 rue de la paix']);
+
+        /** @var(Client) */
+        $user = $user->object();
+        $I->amLoggedInAs($user);
+
+        $I->amOnRoute('app_client_informations');
+        $I->seeInField('form input#user_lastname.champs', 'Ledoux');
+        $I->seeInField('form input#user_firstname.champs', 'Simon');
+        $I->seeInField('form input#user_email.champs', 'simon2@simon511000.fr');
+        $I->seeInField('form input#user_phone', '0654685216');
+        $I->seeInField('form input#user_city', 'Paris');
+        $I->seeInField('form input#user_zipcode', '75000');
+        $I->seeInField('form input#user_address', '1 rue de la paix');
+        //$I->seeInField('#user_birthdate_day option[selected]', 16);
+    }
 }
