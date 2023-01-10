d4463b04c47487778e3b0d41287fffa58cae4107
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-10 22:03:33 +0100

    Nouveau test vérifiant que les données sont bien modifiés

--- tests/Controller/ClientInformations/indexCest.php
+++ tests/Controller/ClientInformations/indexCest.php
@@ -40,6 +40,40 @@ class IndexCest
         $I->seeInField('form input#user_city', 'Paris');
         $I->seeInField('form input#user_zipcode', '75000');
         $I->seeInField('form input#user_address', '1 rue de la paix');
-        //$I->seeInField('#user_birthdate_day option[selected]', 16);
+        // $I->seeInField('#user_birthdate_day option[selected]', 16);
+    }
+
+    // Modifier les données et vérifier que celles-ci sont bien modifiés
+    public function checkPersistanceDonnées(ControllerTester $I)
+    {
+        // Création d'un utilisateur Client
+        $user = ClientFactory::createOne(['lastname' => 'Ledoux', 'firstname' => 'Simon', 'email' => 'simon3@simon511000.fr', 'roles' => ['ROLE_CLIENT'], 'password' => 'password', 'phone' => '0654685216', 'birthdate' => new \DateTime('1970-01-01'), 'city' => 'Paris', 'zipcode' => '75000', 'address' => '1 rue de la paix']);
+
+        /** @var(Client) */
+        $user = $user->object();
+
+        $I->amLoggedInAs($user);
+        $I->amOnRoute('app_client_informations');
+
+        // Modifier le client avec de nouvelles données
+        $I->submitForm('form[name=user]', [
+            'user[lastname]' => 'Leroi',
+            'user[firstname]' => 'Michel',
+            'user[email]' => 'leroi.michel@example.com',
+            'user[phone]' => '0614756412',
+            'user[birthdate]' => '1985-12-31',
+            'user[city]' => 'MichelCity',
+            'user[zipcode]' => '17645',
+            'user[address]' => '8 rue des Michels',
+        ]);
+
+        // Regarder si les modifications ont été effectuées
+        $I->seeInField('form input#user_lastname.champs', 'Leroi');
+        $I->seeInField('form input#user_firstname.champs', 'Michel');
+        $I->seeInField('form input#user_email.champs', 'leroi.michel@example.com');
+        $I->seeInField('form input#user_phone', '0614756412');
+        $I->seeInField('form input#user_city', 'MichelCity');
+        $I->seeInField('form input#user_zipcode', '17645');
+        $I->seeInField('form input#user_address', '8 rue des Michels');
     }
 }
