8a85a2c989ef80b9daf9de88b58d1cc1562d23b5
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-10 16:25:37 +0100

    Ajout du crud pour les RDV

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -28,19 +28,78 @@ class EventCrudController extends AbstractCrudController
                 ->setLabel('Animal')
                 ->setFormTypeOption('choice_label', function($choice, $key, $value) {
                     return (
-                        $choice->client->getLastName()
+                        $choice->getClient()->getLastName()
                         . ' '
-                        . $choice->client->getFirstName()
-                    )
+                        . $choice->getClient()->getFirstName()
+                        . ' - '
+                        . $choice->getName()
+                    );
                 })
                 ->setFormTypeOption('query_builder', function (EntityRepository $entityRepository) {
                     return $entityRepository->createQueryBuilder('animal')
                         ->addSelect('client')
                         ->innerJoin('animal.client', 'client')
-                        ->orderBy('animal.name', 'ASC');
+                        ->orderBy('client.lastname', 'ASC')
+                        ->addOrderBy('client.firstname', 'ASC');
                 })
                 ->formatValue(function ($value, Event $entity) {
                     return $entity->getAnimal()->getName();
+                }),
+            AssociationField::new('animal')
+                ->setLabel('Client')
+                ->hideOnForm()
+                ->setVirtual(true)
+                ->setFormTypeOption('choice_label', function($choice, $key, $value) {
+                    return (
+                        $choice->getClient()->getLastName()
+                        . ' '
+                        . $choice->getClient()->getFirstName()
+                    );
+                })
+                ->setFormTypeOption('query_builder', function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('animal')
+                        ->addSelect('client')
+                        ->innerJoin('animal.client', 'client')
+                        ->orderBy('client.lastname', 'ASC')
+                        ->addOrderBy('client.firstname', 'ASC');
+                })
+                ->formatValue(function ($value, Event $entity) {
+                    return (
+                        $entity->getAnimal()->getClient()->getLastname()
+                        . ' '
+                        . $entity->getAnimal()->getClient()->getFirstname()
+                    );
+                }),
+            AssociationField::new('typeEvent')
+                ->setLabel('Type')
+                ->setFormTypeOption('choice_label', 'libtype')
+                ->setFormTypeOption('query_builder', function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('typeEvent')
+                        ->orderBy('typeEvent.libType', 'ASC');
+                })
+                ->formatValue(function ($value, Event $entity) {
+                    return $entity->getTypeEvent()->getLibType();
+                }),
+            AssociationField::new('veterinaire')
+                ->setLabel('Vétérinaire')
+                ->setFormTypeOption('choice_label', function($choice, $key, $value) {
+                    return (
+                        $choice->getLastName()
+                        . ' '
+                        . $choice->getFirstName()
+                    );
+                })
+                ->setFormTypeOption('query_builder', function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('veterinaire')
+                        ->orderBy('veterinaire.lastname', 'ASC')
+                        ->addOrderBy('veterinaire.firstname', 'ASC');
+                })
+                ->formatValue(function ($value, Event $entity) {
+                    return (
+                        $entity->getVeterinaire()->getLastname()
+                        . ' '
+                        . $entity->getVeterinaire()->getFirstname()
+                    );
                 }),
         ];
     }
