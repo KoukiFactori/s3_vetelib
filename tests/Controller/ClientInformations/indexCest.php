4c261fa17e8d354b5e500227fc4b431712353ee2
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-10 15:56:59 +0100

    Debut de création de tests

--- tests/Controller/ClientInformations/indexCest.php
+++ tests/Controller/ClientInformations/indexCest.php
@@ -1,18 +1,22 @@
 <?php
 
-
 namespace App\Tests\Controller\ClientInformations;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class indexCest
 {
-    public function _before(ControllerTester $I)
+    // Vérifier le bon chargement de la page
+    public function chargementPage(ControllerTester $I)
     {
-    }
+        // Création d'un utilisateur Client
+        $user = UserFactory::createOne(['lastname' => 'Ledoux', 'firstname' => 'Simon', 'email' => 'simon@simon511000.fr', 'roles' => ['ROLE_CLIENT'], 'password' => 'password','phone' => '0654685216', 'birthdate' => new \cr, 'city' => 'Paris', 'zipcode' => '75000', 'address' => '1 rue de la paix']);
 
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
+        $user = $user->object();
+        $I->amLoggedInAs($user);
+
+        $I->amOnPage('/client-informations');
+        $I->seeResponseCodeIs(200);
     }
 }
