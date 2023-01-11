dd4023712bcbd928cbed5291aee01f551d24de1f
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2023-01-11 13:34:08 +0100

    Séparations des types d'utilisateurs dans l'administration

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Admin;
 use App\Entity\Animal;
+use App\Entity\Client;
 use App\Entity\Espece;
 use App\Entity\Event;
 use App\Entity\TypeEvent;
 use App\Entity\User;
+use App\Entity\Veterinaire;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -32,7 +35,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Administrateurs', 'fa fa-user', Admin::class);
+        yield MenuItem::linkToCrud('Vétérinaires', 'fa fa-user', Veterinaire::class);
+        yield MenuItem::linkToCrud('Clients', 'fa fa-user', Client::class);
         yield MenuItem::linkToCrud('Rendez-vous', 'fa fa-calendar-days', Event::class);
         yield MenuItem::linkToCrud('Types de RDV', 'fa fa-list', TypeEvent::class);
         yield MenuItem::linkToCrud('Animaux', 'fa fa-cat', Animal::class);
---
+++ src/Controller/Admin/VeterinaireCrudController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Veterinaire;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[IsGranted('ROLE_ADMIN')]
+class VeterinaireCrudController extends AbstractCrudController
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return Veterinaire::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('email'),
+            TextField::new('firstname')
+                ->setLabel('Prénom'),
+            TextField::new('lastname')
+                ->setLabel('Nom'),
+            TextField::new('phone')
+                ->setLabel('Numéro de Téléphone')
+                ->setRequired(false),
+            DateField::new('birthdate')
+                ->setLabel('Date de naissance')
+                ->setRequired(true),
+            TextField::new('city')
+                ->setLabel('Ville'),
+            TextField::new('zipcode')
+                ->setLabel('Code Postal'),
+            TextField::new('address')
+                ->setLabel('Addresse'),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->onlyOnForms()
+                ->setFormTypeOptions([
+                    'required' => false,
+                    'empty_data' => '',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ]),
+        ];
+    }
+
+    private function setUserPassword($entityInstance): void
+    {
+        $password = $this->getContext()->getRequest()->get('Veterinaire')['password'];
+
+        if ('' != $password) {
+            $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $password));
+        }
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        $entityInstance->setRoles(['ROLE_VETERINAIRE']);
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+}
